aaacace7008e9dca688ef0ffe34dfc9549d8031e
Darwin589 <chucochicho2009@gmail.com>
2026-02-02 12:39:18 -0500

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,27 @@
+//Darwin Castillo
+import java.util.ArrayList;
+public class Practice{
+
+public static void main(String[]args){
+Integer i= new Integer(30);
+Double d = new Double(40.3);
+System.out.println(Integer.MIN_VALUE);
+System.out.println(Integer.MAX_VALUE);
+System.out.println(d*i);
+int newInt = i;
+double newDouble =d;
+
+ArrayList <String> list = new ArrayList <String>();
+list.add("Micheal");
+list.add("Jessica");
+list.add("Lee");
+list.add(1,"Sarah");
+System.out.println(list);
+String store = list.set(2,"Mary");
+String store2 = list.get(3);
+System.out.println(store2);
+String store3 = list.remove(1);
+int s = list.size();
+System.out.println(list);
+}
+}
